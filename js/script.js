944e0807b941ebabc3f7f0e601be56f8edaa62e1
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2020-05-27 15:36:06 +0300

    Small update comment the firs day

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // Комментарии написаны на Русском в учебных целях для личного использования !
 
-// - Меню
+// - 1ый день
+
+// > Меню
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
 
@@ -12,21 +14,22 @@ hamburger.addEventListener('click', () => { // addEventListener - (вызов ф
 
 // Закрытия меню когда мы щелкаем за ее пределами
 document.addEventListener('click', event => {
-	const target = event.target;
+	const target = event.target; // target назначаем event.target для того что бы не писать ниже event.target (хорошая практика)
 	if (!target.closest('.left-menu')) { // стоит ! знак отрицания что бы поменять true на false. А когда стоит !! это двойное отрицание что бы посмотреть буливое значение элемента
 		leftMenu.classList.remove('openMenu'); // Метод remove позволяет закрыть окно (убрать классы openMenu и open)
 		hamburger.classList.remove('open');
 	}
 });
-
-leftMenu.addEventListener('click', event => {
+// Drop Down Menu делает выпадающие списки по нажатию в левом окне - меню (раскрывает список ul "dropdown-list")
+leftMenu.addEventListener('click', event => { // event без скобок () так как когда 1 элемент то скобки не обязательны
 	const target = event.target;
 	const dropdown = target.closest ('.dropdown'); // closest - это метод который постепенно подымается по элементам пока не найдет указанный селектор. Если не найдет то возвращает "null"
 	if (dropdown) {
 		dropdown.classList.toggle('active');
-		leftMenu.classList.add('openMenu');
+		leftMenu.classList.add('openMenu'); // 29 и 30 строчки добавляется классы для того что бы меню открывалось по нажатию на иконки (не обязательно нажимать на гамбургер что бы открыть меню)
 		hamburger.classList.add('open');
 	}
 });
 
-// - 2 день
+// - 2ой день
+
